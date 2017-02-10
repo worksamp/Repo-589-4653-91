5c543619464e420e47644c9f37904e551a047ddd
worksamp <work.samp01@gmail.com>
2017-02-10 14:29:24 +0530

    Update EmployeeController.java

--- spr-mvc-hib-master/src/main/java/com/hiresmart/controller/EmployeeController.java
+++ spr-mvc-hib-master/src/main/java/com/hiresmart/controller/EmployeeController.java
@@ -42,6 +42,7 @@ public class EmployeeController {
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public ModelAndView addEmployee(@ModelAttribute("command")  EmployeeBean employeeBean,
 			BindingResult result) {
+		int x;
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("employees",  prepareListofBean(employeeService.listEmployeess()));
 		return new ModelAndView("addEmployee", model);
